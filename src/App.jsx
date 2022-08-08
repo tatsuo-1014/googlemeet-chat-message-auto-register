6c74022cbc0a58b1e654e08863f253632cd23024
t_nakajima <t_nakajima@bbmedia.co.jp>
2022-08-08 19:31:52 +0900

    add headings component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Heading from './modules/atoms/Heading/index'
+import Label from "./modules/atoms/Label";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    // const [count, setCount] = useState(0)
+    return (
+        <div className="App">
+            {/*<Label children={"labels"}/>*/}
+            <Heading tag={"h1"} visualLevel={2} className={'test'}>visualLevel2</Heading>
+            <Heading>test</Heading>
+        </div>
+    )
 }
 
 export default App
---
+++ src/modules/atoms/Heading/index.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import headingStyle from '../../../assets/scss/atoms/heading.module.css'
+
+/**
+ *
+ * @param Tag
+ * @param visualLevel
+ * @param className
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const HeadingPresenter = ({
+                                     tag: Tag,
+                                     visualLevel,
+                                     className,
+                                     ...props
+                                 }) => (
+    <Tag className={['c-heading',headingStyle[`level--${visualLevel}`],className].join(' ')}
+         {...props}
+    />
+)
+
+/**
+ *
+ * @param presenter
+ * @param level
+ * @param visualLevel
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+export const HeadingContainer = ({
+                                     presenter,
+                                     level = 2,
+                                     visualLevel,
+                                     ...props
+                                 }) => {
+    level = Math.max(1, Math.min(6, level))
+    visualLevel = (typeof visualLevel !== undefined) ? visualLevel : level
+    const tag = `div`;
+    return presenter({tag, visualLevel, ...props})
+}
+
+/**
+ *
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const Heading = props => {
+    return (
+        <HeadingContainer presenter={parentProps => <HeadingPresenter {...parentProps}/>} {...props}/>
+    )
+}
+
+export default Heading

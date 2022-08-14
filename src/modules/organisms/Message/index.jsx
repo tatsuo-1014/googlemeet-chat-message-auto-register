4a6cce6393bf0995ef93e33869d239de0c9a9c57
t_nakajima <t_nakajima@bbmedia.co.jp>
2022-08-14 18:07:12 +0900

    linkに対応

--- src/modules/organisms/Message/index.jsx
+++ src/modules/organisms/Message/index.jsx
@@ -5,11 +5,12 @@ import styles from "./message.module.scss"
 import List from "../List";
 
 export const Message = ({meets,className,meetTime,...props}) => {
-
     // let meet = []
     const meet = meets.filter((meet)=>{return meet.time === meetTime})
+    const meetTitle = meet[0].meetTitle
     const messageBlocks = meet[0].messageBlocks
     const messages = messageBlocks.map(contents=>contents["messages"])
+    const urlPattern = /^(https?|ftp)(:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+)/
     // const messages = JSON.stringify(meet.messageBlocks[0].messages)
     // const messageBlocks = meet.messageBlocks[0].messages
     const handleClick = () =>{
@@ -25,7 +26,7 @@ export const Message = ({meets,className,meetTime,...props}) => {
         <div className={[styles.message,className].join(' ')}>
             <div className={[styles.message__inner]}>
                 <div className={'content'}>
-                    <Heading tag={'h2'} visualLevel={3} >meetID</Heading>
+                    {/*<Heading tag={'h2'} visualLevel={3} >{meetTitle}</Heading>*/}
                     <div className={'messageBlocks'} onClick={()=>{handleClick()}}>
                         {/*<Txt size={"s"} className={[styles.messageBlock]}>test</Txt>*/}
                         {messageBlocks.map(contents => (
@@ -36,9 +37,13 @@ export const Message = ({meets,className,meetTime,...props}) => {
                                 </div>
                                 {/*<p>{contents['messages']}</p>*/}
                                 <div>
-                                    {contents['messages'].map(comment=>(
-                                        <Txt size={'s'}>{comment}</Txt>
-                                    ))}
+                                    {contents['messages'].map(comment=>{
+                                        if(urlPattern.test(comment) == true){
+                                            return <a href={comment} target={"_blank"}><Txt>{comment}</Txt></a>
+                                        }else{
+                                            return <Txt>{comment}</Txt>
+                                        }
+                                    })}
                                 </div>
                             </div>
                             // contents['messages']
